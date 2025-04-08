6a443dcab7df1e3de04841559fb2b3d20904311c
SuperJRM <163477766+SuperJRM@users.noreply.github.com>
2025-04-07 23:01:20 -0700

    Inserted Glitch Generator

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -7,28 +7,61 @@
 // Constants - User-servicable parts
 // In a longer project I like to put these in a separate file
 
-// define a class
-class MyProjectClass {
-  // constructor function
-  constructor(param1, param2) {
-    // set properties using 'this' keyword
-    this.property1 = param1;
-    this.property2 = param2;
-  }
-  
-  // define a method
-  myMethod() {
-    // code to run when method is called
-  }
-}
 
 function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
-
-  // call a method on the instance
-  myInstance.myMethod();
+  const fillers = {
+    player: ["Player,", "", "Adventurer,", "Snake,", "Dragonborn,", "Tarnished,", "Helldivers,", "Crewmate,", "Hero,"],
+    alert: ["Remember,", "Be careful!", "Watch out!", "Did you know?", "Check this out.", "Have you heard?", "Listen up."],
+    action: ["Saving", "Wavedashing", "Blocking", "Countering", "Fighting", "Remembering", "Struggling", "Learning", "Failing", "Dodging", "Reloading", "Gaming", "Winning", "Freedom"],
+    description: ["the difference between life and death", "important", "lifesaving", "less than important", "a lifestyle", "everything", "unimportant", "a requirement", "tedious", "nauseating", "epic"],
+    goal: ["fighting", "living", "playing", "winning", "failing", "losing", "blocking", "saving", "countering", "surviving", "remembering", "running"],
+    advice: ["fight harder", "get good", "buy microtransactions", "do more", "block", "counter", "save money", "watch your health", "keep calm", "stay alive", "hide", "pierce it with a high-performance HEAT (high-explosive, antitank) round", "run", "save"],
+    primaries: ["counters", "guards", "blocking", "men", "chumps", "winners", "the government", "society", "spacies", "sword wielders", "Helldivers", "heroes", "the chosen one", "guns", "war", "you"],
+    verb: ["beat", "wield", "wear", "defeat", "counter", "drink", "heal", "revive", "find", "save", "override", "ride", "restore"],
+    secondaries: ["guards", "counters", "blocks", "mankind", "Super Earth", "the Storm", "the Monado", "the keyblade", "greatswords", "society", "tanks", "demons", "enemies", "Metal Gear", "Bowser"],
+    target: ["Super-Earth", "Bowser", "Otacon", "Ranni", "Society", "Mankind", "Kazuya Mishima", "The Door to Darkness", "Dr. Eggman", "The imposter", "The princess", "Luigi", "The player", "Donkey Kong", "The Pope"],
+    message: ["counting on you", "waiting", "fighting back", "lying in wait", "reporting for duty", "watching", "a lie", "our last hope", "requesting backup", "sending in reinforcements"],
+    
+  };
+  
+  const template = `
+  $player $alert
+  
+  $action isn't just important, it's $description. 
+  
+  To keep $goal, try to $advice.
+  
+  Only $primaries can $verb $secondaries. 
+  
+  $target is $message!
+  `;
+  
+  const slotPattern = /\$(\w+)/;
+  
+  function replacer(match, name) {
+    let options = fillers[name];
+    if (options) {
+      return options[Math.floor(Math.random() * options.length)];
+    } else {
+      return `<UNKNOWN:${name}>`;
+    }
+  }
+  
+  function generate() {
+    let story = template;
+    while (story.match(slotPattern)) {
+      story = story.replace(slotPattern, replacer);
+    }
+  
+    /* global box */
+    box.innerText = story;
+  }
+  
+  /* global clicker */
+  clicker.onclick = generate;
+  
+  generate();
+  
 }
 
-// let's get this party started - uncomment me
-//main();
+main();
